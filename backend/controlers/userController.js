2dbd8f48355de0e1a92c5d32a4216def0d6722c5
shubham kumar <kumarshubham417@gmail.com>
2023-04-13 20:04:27 +0530

    user register functionality implimented

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -1,21 +1,46 @@
 const { userModel } = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const getUser = (req, res) => {
   res.status(200).json({ message: "users" });
 };
-const registerUser = async (req, res) => {
-  let { name, email, password } = req.body;
 
+//to register
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await userModel.findOne({ email });
+  if (userExists) {
+    res.status(400).json({ message: "user already exists" });
+  }
   try {
-    const user = new userModel({ name, email, password });
+    let hashedPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({ name, email, password: hashedPassword });
     await user.save();
-    res.status(200).json({ message: "Success" });
+    res.status(201).json({ message: "User Registered successfully" });
   } catch (err) {
     console.log(err);
   }
 };
-const loginUser = (req, res) => {
-  res.status(200).json({ message: "Success" });
+
+//to login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "User not exists" });
+    } else {
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+          res.status(400).json({ message: "Wrong password" });
+        } else {
+            const token = 
+          res.status(200).json({ message: "User logged in successfully" });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { getUser, registerUser, loginUser };
