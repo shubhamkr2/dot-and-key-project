1eabdb8d0e77b10245daf50c2231aed75c479814
shubham kumar <kumarshubham417@gmail.com>
2023-04-13 11:10:23 +0530

    db is configured and connected

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const { connection } = require("./config/db");
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Home");
+  res.status(200).json({ Message: "Welcome to dotapp Home" });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log(err);
+  }
   console.log(`Express server listening on PORT ${process.env.PORT}`);
 });
