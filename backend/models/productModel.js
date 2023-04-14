1302748c1e1b2655fe4342495b2061591b38596a
shubham kumar <kumarshubham417@gmail.com>
2023-04-14 10:16:30 +0530

    product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,2 +1,23 @@
+const mongoose = require("mongoose");
 
+const productSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+});
 
+const productModel = mongoose.model("product", productSchema);
+module.exports = { productModel };
