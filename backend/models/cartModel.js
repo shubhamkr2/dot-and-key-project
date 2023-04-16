0b7e82099ae7cc68a58024e18df1feb283fbd59d
shubham kumar <kumarshubham417@gmail.com>
2023-04-16 23:48:19 +0530

    cart model is created

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,2 +1,39 @@
+const mongoose = require("mongoose");
 
+const cartSchema = mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Boolean,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
 
+const CartModel = mongoose.model("carts", cartSchema);
+module.exports = { CartModel };
