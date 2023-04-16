c2289bb817c929f186757958df3ca82ca0f2b21e
shubham kumar <kumarshubham417@gmail.com>
2023-04-16 23:54:43 +0530

    order model is created

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,2 +1,43 @@
+const mongoose = require("mongoose");
 
+const orderSchema = mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Boolean,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
 
+const OrderModel = mongoose.model("carts", orderSchema);
+module.exports = { OrderModel };
