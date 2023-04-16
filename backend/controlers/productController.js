c61cbf78c3877ab8f463ebcf8a54fdf79f882228
shubham kumar <kumarshubham417@gmail.com>
2023-04-16 14:57:10 +0530

    product controler updated for product updating

--- backend/controlers/productController.js
+++ backend/controlers/productController.js
@@ -8,7 +8,7 @@ const getProducts = async (req, res) => {
     let isPage = req.query?.page !== undefined;
     let isLimit = req.query?.limit !== undefined;
     let isSort = req.query?.sortBy !== undefined;
-    
+
     //Storing requested query
     let category = req.query.category;
     let page = +req.query?.page;
@@ -103,11 +103,20 @@ const addProducts = async (req, res) => {
   }
 };
 
-const updateProducts = (req, res) => {
-  res.status(200).json({ message: "updated product" });
+const updateProducts = async (req, res) => {
+  try {
+    let params = req.params;
+    await ProductModel.findByIdAndUpdate(params,req.body);
+    console.log(req.body);
+    res.status(200).json({ message: "updated product" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Unable to update the product" });
+  }
 };
 
 const deleteProducts = (req, res) => {
+
   res.status(200).json({ message: "deleted product" });
 };
 module.exports = {
