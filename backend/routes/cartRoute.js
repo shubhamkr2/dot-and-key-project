38a94a695284eabe583bb54c685d5117d013f0cc
shubham kumar <kumarshubham417@gmail.com>
2023-04-17 09:45:56 +0530

    cart route created

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,2 +1,20 @@
+const { Router } = require("express");
+const {
+    getCartItems,
+    getItemByID,
+    addItems,
+    updateItems,
+    deleteItems,
+} = require("../controlers/productController");
+
+const cartRoute = Router();
+
+cartRoute.get("/", getCartItems);
+cartRoute.get("/:id", getItemByID);
+cartRoute.post("/", addItems);
+cartRoute.put("/:id", updateItems);
+cartRoute.delete("/:id", deleteItems);
+
+module.exports = { cartRoute };
 
 
