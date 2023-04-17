c023583124d446b04fe5fb214b17cb6a4c670ffe
shubham kumar <kumarshubham417@gmail.com>
2023-04-17 10:02:44 +0530

    order route created

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,2 +1,22 @@
 
 
+const { Router } = require("express");
+const {
+    getCartItems,
+    getItemByID,
+    addItems,
+    updateItems,
+    deleteItems,
+} = require("../controlers/orderController");
+
+const orderRoute = Router();
+
+orderRoute.get("/", getCartItems);
+orderRoute.get("/:id", getItemByID);
+orderRoute.post("/", addItems);
+orderRoute.put("/:id", updateItems);
+orderRoute.delete("/:id", deleteItems);
+
+module.exports = { orderRoute };
+
+
