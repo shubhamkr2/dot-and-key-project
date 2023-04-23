fd7c1c284b11a16942f4cb09ca0ed726b664e9be
shubham kumar <kumarshubham417@gmail.com>
2023-04-24 00:07:28 +0530

    login page updated

--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Styles/LogIn.module.css";
 import { Link } from "react-router-dom";
 
+const initialFormData = {
+  email: "",
+  password: "",
+};
+
 function LogIn() {
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+    console.log(formData);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className={styles.container}>
       <h2>Log In</h2>
       <div className={styles.form_container}>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div>
-            <label>EMAIL</label>
-            <input type="text" placeholder="Enter your email" />
+            <label className={styles.required}>EMAIL</label>
+            <input type="text" placeholder="Enter your email" name="email" value={formData.email} onChange={(e) => handleChange(e)} required />
           </div>
           <div>
-            <label>PASSWORD</label>
-            <input type="password" placeholder="Enter your password" />
+            <label className={styles.required}>PASSWORD</label>
+            <input type="password" placeholder="Enter your password" name="password" value={formData.password} onChange={(e) => handleChange(e)} required />
             <span className={styles.forgot_password}><Link>Forgot Password?</Link></span>
           </div>
           <button>Log in</button>
