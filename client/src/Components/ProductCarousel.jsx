0a447b5a589676e8e137c4e6451c450a530c9f72
shubham kumar <kumarshubham417@gmail.com>
2023-04-30 22:47:48 +0530

    product carousel added

--- client/src/Components/ProductCarousel.jsx
+++ client/src/Components/ProductCarousel.jsx
@@ -6,6 +6,7 @@ import { AiOutlineArrowLeft } from "react-icons/ai";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import styles from "../Styles/ProductCarousel.module.css";
+import { Carousel_Product_Card } from "./Carousel_Product_Card";
 
 
 function ProductCarousel({images}) {
@@ -18,21 +19,19 @@ function ProductCarousel({images}) {
       ref.current.slickPrev();
     };
     const settings = {
-      dots: true,
-      infinite: true,
+      dots: false,
+      infinite: false,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 4,
       slidesToScroll: 1,
       arrows: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
     };
   return (
-    <div className={styles.carousel_container}>
+    <div style={{width:"68%",margin:"auto",position:"relative"}}>
     <Slider {...settings} ref={ref}>
-      {images?.map((item) => (
-        <div>
-          <img src={item} style={{ width: "100%" }} alt="Images" />
+      {images?.map((item,i) => (
+        <div key={i}>
+          <Carousel_Product_Card product={item} />
         </div>
       ))}
     </Slider>
@@ -44,9 +43,9 @@ function ProductCarousel({images}) {
         position: "absolute",
         width: "50px",
         height: "150px",
-        left: "0%",
-        top: "35%",
-        // border: "1px solid red",
+        left: "-7%",
+        top: "30%",
+        border: "1px solid gray",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
@@ -62,9 +61,9 @@ function ProductCarousel({images}) {
       onClick={handleNextSlide}
       style={{
         position: "absolute",
-        top: "35%",
-        right: "0%",
-        // border: "1px solid red",
+        top: "30%",
+        right: "-7%",
+        border: "1px solid gray",
         width: "50px",
         height: "150px",
         display: "flex",
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CarouselBanner } from "../Components/CarouselBanner";
 import ProductCarousel from "../Components/ProductCarousel";
 
+
 const images = [
   "https://www.dotandkey.com/cdn/shop/files/pro_desk_552a0e39-53bd-4a71-98da-ceb1a8d819b7_1024x1024.jpg?v=1680865716",
   "https://www.dotandkey.com/cdn/shop/files/vit_c_range_8fe898e8-20cf-48b8-b793-7636da885b89_1024x1024.jpg?v=1682569560",
@@ -9,13 +10,61 @@ const images = [
   "https://www.dotandkey.com/cdn/shop/files/CATE_DESK_4375de5a-9172-48e3-9308-938dcbe6759b_1024x1024.jpg?v=1681794124",
 ];
 const cards=[
-  "1","2","3","4","5","6"
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  },
+  {
+    image:"https://www.dotandkey.com/cdn/shop/files/vitcsunscreen_large.jpg?v=1682746364",
+    rating:4.8,
+    name:"Vitamin C + E SPF 50 Sunscreen",
+    highlight:"WATERLIGHT TEXTURE",
+    price:495.00,
+  }
 ]
 function Home() {
   return (
     <div>
       <CarouselBanner images={images} />
-      {/* <ProductCarousel images={images} /> */}
+      <ProductCarousel images={cards} />
     </div>
   );
 }
