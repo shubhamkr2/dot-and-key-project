7540e74964de20ac8f13793d4b91f61e6b45350c
shubham kumar <kumarshubham417@gmail.com>
2023-05-01 00:02:49 +0530

    updated product carousel

--- client/src/Components/ProductCarousel.jsx
+++ client/src/Components/ProductCarousel.jsx
@@ -8,76 +8,81 @@ import { IconContext } from "react-icons";
 import styles from "../Styles/ProductCarousel.module.css";
 import { Carousel_Product_Card } from "./Carousel_Product_Card";
 
+function ProductCarousel({ images }) {
+  const ref = useRef(null);
+  const handleNextSlide = () => {
+    ref.current.slickNext();
+  };
 
-function ProductCarousel({images}) {
-    const ref = useRef(null);
-    const handleNextSlide = () => {
-      ref.current.slickNext();
-    };
-  
-    const handlePrevSlide = () => {
-      ref.current.slickPrev();
-    };
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: false,
-    };
+  const handlePrevSlide = () => {
+    ref.current.slickPrev();
+  };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+  };
   return (
-    <div style={{width:"68%",margin:"auto",position:"relative"}}>
-    <Slider {...settings} ref={ref}>
-      {images?.map((item,i) => (
-        <div key={i}>
-          <Carousel_Product_Card product={item} />
-        </div>
-      ))}
-    </Slider>
+    <div
+      style={{
+        width: "80%",
+        margin: "auto",
+        position: "relative",
+      }}
+    >
+      <Slider {...settings} ref={ref}>
+        {images?.map((item, i) => (
+          <div key={i}>
+            <Carousel_Product_Card product={item} />
+          </div>
+        ))}
+      </Slider>
 
-    <div
-      className="button"
-      onClick={handlePrevSlide}
-      style={{
-        position: "absolute",
-        width: "50px",
-        height: "150px",
-        left: "-7%",
-        top: "30%",
-        border: "1px solid gray",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      <IconContext.Provider value={{ size: "2rem" }}>
-        <AiOutlineArrowLeft />
-      </IconContext.Provider>
+      <div
+        className="button"
+        onClick={handlePrevSlide}
+        style={{
+          position: "absolute",
+          width: "50px",
+          height: "150px",
+          left: "-7%",
+          top: "30%",
+          border: "1px solid gray",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        <IconContext.Provider value={{ size: "2rem" }}>
+          <AiOutlineArrowLeft />
+        </IconContext.Provider>
+      </div>
+      <div
+        className="button"
+        onClick={handleNextSlide}
+        style={{
+          position: "absolute",
+          top: "30%",
+          right: "-3%",
+          border: "1px solid gray",
+          width: "50px",
+          height: "150px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        <IconContext.Provider value={{ size: "2rem" }}>
+          <AiOutlineArrowRight />
+        </IconContext.Provider>
+      </div>
     </div>
-    <div
-      className="button"
-      onClick={handleNextSlide}
-      style={{
-        position: "absolute",
-        top: "30%",
-        right: "-7%",
-        border: "1px solid gray",
-        width: "50px",
-        height: "150px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        cursor: "pointer",
-      }}
-    >
-      <IconContext.Provider value={{ size: "2rem" }}>
-        <AiOutlineArrowRight />
-      </IconContext.Provider>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default ProductCarousel
+export default ProductCarousel;
