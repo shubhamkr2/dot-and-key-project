198b428c9bf2c0c0cfec257979a0780462734332
shubham kumar <kumarshubham417@gmail.com>
2023-05-03 22:24:17 +0530

    navbar fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import { Navbar } from "./Components/Navbar";
-import { NavigationBar } from "./Components/NavigationBar";
 import { AllRoutes } from "./Routes/AllRoutes";
 
 function App() {
   return (
     <div>
     <Navbar />
-    <NavigationBar />
       <AllRoutes />
     </div>
   );
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
       </div>
       </div>
     </div>
-    {/* <NavigationBar /> */}
+    <NavigationBar />
     </>
   );
 }
