f3f10235d8efb0245f090ce715dd7cd9fa1731bb
shubham kumar <kumarshubham417@gmail.com>
2023-05-09 17:55:39 +0530

    sunscreens banner added

--- client/src/Pages/Sunscreens.jsx
+++ client/src/Pages/Sunscreens.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../Styles/Sunscreens.module.css";
+import { NavigationBar } from "../Components/NavigationBar";
 function Sunscreens() {
   return (
-    <div>Sunscreens</div>
-  )
+    <div className={styles.sunscreens_container}>
+      <NavigationBar />
+      <img
+        className={styles.banner}
+        src="https://www.dotandkey.com/cdn/shop/collections/CATE-DESK_1_1512x.jpg?v=1682595235"
+        alt="sunscreens"
+      />
+    </div>
+  );
 }
 
 export { Sunscreens };
