9a95e1d83b47bc87fe25ee2c78a83a82ad9c0927
shubham kumar <kumarshubham417@gmail.com>
2023-05-12 14:49:58 +0530

    action created to get all product

--- client/src/Pages/Sunscreens.jsx
+++ client/src/Pages/Sunscreens.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../Styles/Sunscreens.module.css";
 import { NavigationBar } from "../Components/NavigationBar";
+import { useDispatch } from "react-redux";
+import { getAllProduct } from "../Redux/actions/product.action";
+
 function Sunscreens() {
+  const dispatch = useDispatch();
+useEffect(()=>{
+  dispatch(getAllProduct())
+})
   return (
     <div className={styles.sunscreens_container}>
       <NavigationBar />
--- client/src/Redux/actions/product.action.js
+++ client/src/Redux/actions/product.action.js
@@ -7,7 +7,22 @@ import {
   SINGLE_PRODUCTS_FAIL,
 } from "../actionTypes/product.actionTypes.js";
 
-function getAllProduct() {}
+function getAllProduct() {
+  return async function (dispatch) {
+    dispatch({ type: ALL_PRODUCTS_REQUEST });
+    try {
+      let res = await fetch(
+        `https://courageous-rose-nightgown.cyclic.app/products`
+      );
+      let data = await res.json();
+      dispatch({ type: ALL_PRODUCTS_SUCCESS, payload: data });
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: ALL_PRODUCTS_FAIL });
+    }
+  };
+}
 function getSingleProduct() {}
 
 export { getAllProduct, getSingleProduct };
