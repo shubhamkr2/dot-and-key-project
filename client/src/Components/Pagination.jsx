01191dff685da3606be117dc4f43c039a8c51ade
shubham kumar <kumarshubham417@gmail.com>
2023-05-15 22:37:45 +0530

    pagination component

--- client/src/Components/Pagination.jsx
+++ client/src/Components/Pagination.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "../Styles/Pagination.module.css";
 
 function Pagination({page, setPage, data}) {
+  const totalPage=Math.ceil(data?.count / 6);
   return (
     <div className={styles.pagination}>
-      <button onClick={() => setPage(page - 1)}>Prev</button>
+      <button disabled={page<2} className={styles.prev_btn} onClick={() => setPage(page - 1)}>Prev</button>
       <span>
-        {page}/{Math.ceil(data?.count / 6)}
+        {page}/{totalPage}
       </span>
-      <button onClick={() => setPage(page + 1)}>Next</button>
+      <button disabled={page===totalPage} className={styles.next_btn} onClick={() => setPage(page + 1)}>Next</button>
     </div>
   );
 }
