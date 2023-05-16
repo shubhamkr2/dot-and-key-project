825d8b404570797c8e1021489d16cf95c3cd717b
shubham kumar <kumarshubham417@gmail.com>
2023-05-16 17:23:53 +0530

    skeleton updated

--- client/src/Components/Skeleton.jsx
+++ client/src/Components/Skeleton.jsx
@@ -3,13 +3,32 @@ import styles from "../Styles/Skeleton.module.css";
 
 const Skeleton = () => {
   return (
+    
     <div className={styles.container}>
+    <div>
       <div className={`${styles.skeleton} ${styles.skeletonImage}`} />
       <div className={`${styles.skeleton} ${styles.skeletonRating}`} />
       <div className={`${styles.skeleton} ${styles.skeletonTitle}`} />
       <div className={`${styles.skeleton} ${styles.skeletonHighlights}`} />
       <div className={`${styles.skeleton} ${styles.skeletonPrice}`} />
       <div className={`${styles.skeleton} ${styles.skeletonButton}`} />
+    </div>
+    <div>
+      <div className={`${styles.skeleton} ${styles.skeletonImage}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonRating}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonTitle}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonHighlights}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonPrice}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonButton}`} />
+    </div>
+    <div>
+      <div className={`${styles.skeleton} ${styles.skeletonImage}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonRating}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonTitle}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonHighlights}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonPrice}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonButton}`} />
+    </div>
     </div>
   );
 };
