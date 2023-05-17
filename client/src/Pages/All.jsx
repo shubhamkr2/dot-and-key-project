14afa2fb69024b3fdb117d1d37f8201ec39b2047
shubham kumar <kumarshubham417@gmail.com>
2023-05-17 18:12:22 +0530

    all page component created

--- client/src/Pages/All.jsx
+++ client/src/Pages/All.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import styles from "../Styles/All.module.css";
+import { NavigationBar } from "../Components/NavigationBar";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProduct } from "../Redux/actions/product.action";
+import { useParams } from "react-router-dom";
+import ProductCard from "../Components/ProductCard";
+import { Footer } from "../Components/Footer";
+import Pagination from "../Components/Pagination";
+import Sidebar from "../Components/Sidebar";
+import { Skeleton } from "../Components/Skeleton";
 
 function All() {
+  const { loading, data } = useSelector((state) => state.product);
+  const [page, setPage] = useState(1);
+  const [sortAs, setSortAs] = useState("");
+  const [filterAsRating, setFilterAsRating] = useState("");
+  const dispatch = useDispatch();
+  const url = useParams();
+
+  useEffect(() => {
+    dispatch(getAllProduct("", page, sortAs, filterAsRating)).then(() => {});
+  }, [page, sortAs, filterAsRating]);
+
+  console.log(data);
   return (
-    <div>All</div>
-  )
+    <div className={styles.all_container}>
+      <NavigationBar />
+      <img
+        className={styles.banner}
+        src="https://www.dotandkey.com/cdn/shop/collections/CATE-DESK_1_1512x.jpg?v=1682595235"
+        alt="sunscreens"
+      />
+      <div className={styles.sidebar_and_product}>
+        <div className={styles.Sidebar}>
+          <Sidebar
+            setSortAs={setSortAs}
+            setFilterAsRating={setFilterAsRating}
+          />
+        </div>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <div className={styles.product_list}>
+            {data?.data?.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))}
+          </div>
+        )}
+      </div>
+      <Pagination page={page} setPage={setPage} data={data} />
+      <Footer />
+    </div>
+  );
 }
 
 export { All };
