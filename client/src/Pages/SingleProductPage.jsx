566ccdc7ca9d4ebc6932e583e211984c025d5ed8
shubham kumar <kumarshubham417@gmail.com>
2023-05-19 15:59:17 +0530

    title,quantity,category added in description

--- client/src/Pages/SingleProductPage.jsx
+++ client/src/Pages/SingleProductPage.jsx
@@ -12,47 +12,63 @@ function SingleProductPage() {
   );
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const dispatch = useDispatch();
-  const {data} = single_product_data||{};
-  const {category,title,description,price,images,rating,highlights,stock} = data||{};
+  const { data } = single_product_data || {};
+  const {
+    category,
+    title,
+    description,
+    price,
+    images,
+    rating,
+    highlights,
+    stock,
+  } = data || {};
   useEffect(() => {
     dispatch(getSingleProduct(id));
   }, []);
-  
+
   const switchImage = (index) => {
     setCurrentImageIndex(index);
   };
-  
+
   console.log(title);
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
     <>
-    <NavigationBar />
-    <h1 className={styles.category}>{category.toUpperCase()}</h1>
-    <div className={styles.single_page_container}>
-    <div className={styles.image_container}>
-      <div className={styles.main_image}>
-        <img
-          src={images[currentImageIndex]}
-          alt="Product"
-        />
-      </div>
-      <div className={styles.thumbnails}>
-        {images?.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt="Thumbnail"
-            onClick={() => switchImage(index)}
-          />
-        ))}
-      </div>
-      </div>
-      <div className={styles.description_container}>
+      <NavigationBar />
+      <h1 className={styles.category}>{category.toUpperCase()}</h1>
+      <div className={styles.single_page_container}>
+        <div className={styles.image_container}>
+          <div className={styles.main_image}>
+            <img src={images[currentImageIndex]} alt="Product" />
+          </div>
+          <div className={styles.thumbnails}>
+            {images?.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt="Thumbnail"
+                onClick={() => switchImage(index)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={styles.description_container}>
           <h1>{title}</h1>
+          <div className={styles.quantity}>
+            <label>Quantity:</label>
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 }
