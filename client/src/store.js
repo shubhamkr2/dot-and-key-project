f31fb48b162b2a2f9a798661255721a7e87fca27
shubham kumar <kumarshubham417@gmail.com>
2023-05-20 16:48:21 +0530

    store updated with search reducer

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,14 @@ import { userReducer } from "./Redux/reducers/user.reducer";
 import { cartReducer } from "./Redux/reducers/cart.reducer";
 import { resetPassword } from "./Redux/reducers/resetPassword.reducer";
 import { productReducer } from "./Redux/reducers/product.reducer";
+import { searchReducer } from "./Redux/reducers/search.reducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   product: productReducer,
   cart: cartReducer,
   reset: resetPassword,
+  search: searchReducer,
 });
 
 const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
