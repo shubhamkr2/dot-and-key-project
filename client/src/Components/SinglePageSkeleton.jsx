3178f003deebd2c21090e539985699838de6ca8c
shubham kumar <kumarshubham417@gmail.com>
2023-05-22 16:05:46 +0530

    single page skeleton created

---
+++ client/src/Components/SinglePageSkeleton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../Styles/Skeleton.module.css";
+
+const Skeleton = () => {
+  return (
+    
+    <div className={styles.container}>
+    <div>
+      <div className={`${styles.skeleton} ${styles.skeletonImage}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonRating}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonTitle}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonHighlights}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonPrice}`} />
+      <div className={`${styles.skeleton} ${styles.skeletonButton}`} />
+    </div>
+    </div>
+  );
+};
+
+export { Skeleton };
