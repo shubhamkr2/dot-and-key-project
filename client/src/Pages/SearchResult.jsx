abace43325a00f9b6b9846cc315052ec86795140
shubham kumar <kumarshubham417@gmail.com>
2023-05-22 15:59:35 +0530

    searchresult page updated

--- client/src/Pages/SearchResult.jsx
+++ client/src/Pages/SearchResult.jsx
@@ -1,35 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "../Styles/SearchResult.module.css";
 import { NavigationBar } from "../Components/NavigationBar";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllProduct } from "../Redux/actions/product.action";
-import { useParams } from "react-router-dom";
 import ProductCard from "../Components/ProductCard";
 import { Footer } from "../Components/Footer";
-import Pagination from "../Components/Pagination";
-import Sidebar from "../Components/Sidebar";
-import { Skeleton } from "../Components/Skeleton";
 
 function SearchResult() {
+  // Get suggestions and loading state from Redux store
   const { suggestions, isLoading } = useSelector((state) => state.search);
-  const [page, setPage] = useState(1);
-  const [sortAs, setSortAs] = useState("");
-  const [filterAsRating, setFilterAsRating] = useState("");
-  const dispatch = useDispatch();
-  const url = useParams();
-
-  useEffect(() => {
-    dispatch(getAllProduct("", page, sortAs, filterAsRating));
-  }, [page, sortAs, filterAsRating]);
 
   return (
     <div className={styles.all_container}>
+      {/* Render the navigation bar */}
       <NavigationBar />
+
       <h1>Search Results</h1>
+
       <div className={styles.sidebar_and_product}>
-        {suggestions?.data?.length<1 ? (
-          <img src="https://github.com/shubhamkr2/UploadImages/blob/main/no_result.gif?raw=true" alt="no result" />
+        {suggestions?.data?.length < 1 ? (
+          // If no results found, display a message or image
+          <img
+            src="https://github.com/shubhamkr2/UploadImages/blob/main/no_result.gif?raw=true"
+            alt="no result"
+          />
         ) : (
+          // Render the list of products
           <div className={styles.product_list}>
             {suggestions?.data?.map((product) => (
               <ProductCard product={product} key={product._id} />
@@ -37,6 +31,8 @@ function SearchResult() {
           </div>
         )}
       </div>
+
+      {/* Render the footer */}
       <Footer />
     </div>
   );
