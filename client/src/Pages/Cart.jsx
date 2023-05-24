f4e686f0a6d5e2cdba984f362648b0efed594212
shubham kumar <kumarshubham417@gmail.com>
2023-05-24 15:13:13 +0530

    cart page structured

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function Cart() {
+  const {} = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
   return (
     <div>
-        <h1>Cart</h1>
-        
+      <h1>Cart</h1>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2></h2>
+          <span>Category: </span>
+          <span>Rs: </span>
+        </div>
+        <div>
+          <select>
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <h3>Gross Total: </h3>
+        <h3>Shipping: </h3>
+        <h3>Discount: </h3>
+        <h3>Total: </h3>
+      </div>
     </div>
-  )
+  );
 }
 
 export { Cart };
