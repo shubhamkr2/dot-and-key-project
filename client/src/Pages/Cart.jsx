91243ec7fb61a5719d0cb3858cbdfaa19451bbef
shubham kumar <kumarshubham417@gmail.com>
2023-05-24 22:00:08 +0530

    cart card linked

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -2,28 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCartItem } from "../Redux/actions/cart.action";
 import { CartCard } from "../Components/CartCard";
+import styles from "../Styles/Cart.module.css";
 
 function Cart() {
   const token = localStorage.getItem("token") || [];
   const [quantity, setQuantity] = useState(3);
-  const {cartItems} = useSelector((state) => state.cart);
+  const { cartItems } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCartItem(token));
-  },[]);
-console.log(cartItems)
+  }, []);
+  console.log(cartItems);
   return (
-    <div>
+    <div className={styles.main_container}>
       <h1>Cart</h1>
-      {cartItems?.data?.map((item)=>(
-        <CartCard key={item._id} item={item} quantity={quantity} setQuantity={setQuantity}/>
-      ))}
-      <div>
-        <h3>Gross Total: </h3>
-        <h3>Shipping: </h3>
-        <h3>Discount: </h3>
-        <h3>Total: </h3>
+      <div className={styles.cards_and_price}>
+        <div className={styles.cards}>
+          {cartItems?.data?.map((item) => (
+            <CartCard
+              key={item._id}
+              item={item}
+              quantity={quantity}
+              setQuantity={setQuantity}
+            />
+          ))}
+        </div>
+        <div className={styles.price}>
+          <div className={styles.priceItem}>
+            <span className={styles.priceLabel}>Total price:</span>
+            <span className={styles.priceValue}>1000</span>
+          </div>
+          <div className={styles.priceItem}>
+            <span className={styles.priceLabel}>Shipping:</span>
+            <span className={styles.priceValue}>50</span>
+          </div>
+          <div className={styles.priceItem}>
+            <span className={styles.priceLabel}>Discount:</span>
+            <span className={styles.priceValue}>100</span>
+          </div>
+          <div className={styles.totalItem}>
+            <span className={styles.totalLabel}>Total:</span>
+            <span className={styles.totalValue}>1150</span>
+          </div>
+        </div>
       </div>
     </div>
   );
