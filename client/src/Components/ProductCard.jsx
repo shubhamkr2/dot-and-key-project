6b2fd5251490a2d3d91d9562b7855ceb28a76520
shubham kumar <kumarshubham417@gmail.com>
2023-05-26 23:58:21 +0530

    product card is updated

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -6,29 +6,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, getCartItems } from "../Redux/actions/cart.action";
 
-// let data = {
-//   _id: "645e1551062c3fbde39b9667",
-//   category: "sunscreens",
-//   title: "Vitamin C + E SPF 50 Sunscreen",
-//   description: [
-//     "2-in-1 glow + protect SPF 50 sunscreen for even-toned, glowing & protected skin every day.",
-//     "Activates Vitamin D receptors, making it beneficial to be in the sun.",
-//     "Infused with Triple Vitamin C & Sicilian Blood Orange to fight dullness & pigmentation while boosting collagen.",
-//     "Powered by UV filters to protect skin against damaging UVA, UVB & blue light rays.",
-//     "Water-light texture that provides full absorbency for an invisible finish on all skin tones with zero white cast.",
-//   ],
-//   price: 470,
-//   images: [
-//     "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/1-3.webp?v=1683526593",
-//     "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/7.webp?v=1683526593",
-//     "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3-1.webp?v=1683526593",
-//     "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/certif.jpg?v=1683526592",
-//   ],
-//   rating: 4.2,
-//   highlights: "WATERLIGHT TEXTURE",
-//   stock: true,
-//   __v: 0,
-// };
+
 function ProductCard({ product, toast }) {
   const dispatch = useDispatch();
   const token = localStorage.getItem("token") || [];
