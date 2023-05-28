b03284d382ed8d23d076e42ba92c9b9279881df2
shubham kumar <kumarshubham417@gmail.com>
2023-05-29 02:53:37 +0530

    navigation bar updated for active link

--- client/src/Components/NavigationBar.jsx
+++ client/src/Components/NavigationBar.jsx
@@ -8,29 +8,36 @@ import { FaceWash } from "../Pages/FaceWash";
 import { FaceMasks } from "../Pages/FaceMasks";
 import { All } from "../Pages/All";
 import styles from "../Styles/NavigationBar.module.css";
+import { useLocation } from 'react-router-dom';
 
 function NavigationBar() {
+  const location = useLocation();
+  const {pathname}=location;
+  const path = pathname.trim().split("/");
+
+  console.log(path[1]);
+
   return (
     <div className={styles.container}>
-      <Link to="/all" element={<All />}>
+      <Link to="/all" element={<All />} className={path[1] === "all" ? styles.activeLink : ''}>
         All
       </Link>
-      <Link to="/sunscreens" element={<Sunscreens />}>
+      <Link to="/sunscreens" element={<Sunscreens />} className={path[1] === "sunscreens" ? styles.activeLink : ''}>
         Sunscreens
       </Link>
-      <Link to="/moisturizers" element={<Moisturizers />}>
+      <Link to="/moisturizers" element={<Moisturizers />} className={path[1] === "moisturizers" ? styles.activeLink : ''}>
         Moisturizers
       </Link>
-      <Link to="/serums" element={<Serums />}>
+      <Link to="/serums" element={<Serums />} className={path[1] === "serums" ? styles.activeLink : ''}>
         Serums
       </Link>
-      <Link to="/lipcare" element={<LipCare />}>
+      <Link to="/lipcare" element={<LipCare />} className={path[1] === "lipcare" ? styles.activeLink : ''}>
         Lip Care
       </Link>
-      <Link to="/facewash" element={<FaceWash />}>
+      <Link to="/facewash" element={<FaceWash />} className={path[1] === "facewash" ? styles.activeLink : ''}>
         Face Wash
       </Link>
-      <Link to="/facemasks" element={<FaceMasks />}>
+      <Link to="/facemasks" element={<FaceMasks />} className={path[1] === "facemasks" ? styles.activeLink : ''}>
         Face Masks
       </Link>
     </div>
