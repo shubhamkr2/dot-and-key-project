3c78d05421a5caa924a67ee6f4f50b5f67397d7f
shubham kumar <kumarshubham417@gmail.com>
2023-05-29 14:40:40 +0530

    loading effect added

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { IconContext } from "react-icons";
 import styles from "../Styles/ProductCard.module.css";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, getCartItems } from "../Redux/actions/cart.action";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function ProductCard({ product, toast }) {
+  // const { isLoading } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const token = localStorage.getItem("token") || [];
+
+  // Add a loading state for the current product card
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     _id,
@@ -21,8 +26,9 @@ function ProductCard({ product, toast }) {
     highlights,
     stock,
   } = product || {};
-  
+
   async function handleAddToCart() {
+    setIsLoading(true); // Set the loading state to true for the current card
     const cart_product = {
       productId: _id,
       category: category,
@@ -39,6 +45,8 @@ function ProductCard({ product, toast }) {
       dispatch(getCartItems(token));
     } catch (err) {
       console.log(err);
+    } finally {
+      setIsLoading(false); // Set the loading state back to false
     }
   }
   // console.log(product);
@@ -56,7 +64,13 @@ function ProductCard({ product, toast }) {
       <h4>{product.title}</h4>
       <h5 className={styles.highlights}>{product.highlights}</h5>
       <h4 className={styles.price}>Rs: {product.price}</h4>
-      <button onClick={handleAddToCart}>ADD TO CART</button>
+      <button onClick={handleAddToCart} disabled={isLoading ? true : false}>
+        {isLoading ? (
+          <BeatLoader color="#FFFFFF" cssOverride={{ margin: "auto" }} />
+        ) : (
+          "ADD TO CART"
+        )}
+      </button>
     </div>
   );
 }
