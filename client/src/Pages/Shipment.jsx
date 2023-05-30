95b78b66c3508cecc79f4f92820fc9f17a967e82
shubham kumar <kumarshubham417@gmail.com>
2023-05-30 17:34:26 +0530

    shipment form created

--- client/src/Pages/Shipment.jsx
+++ client/src/Pages/Shipment.jsx
@@ -1,47 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Styles/Shipment.module.css";
 
+let initialFormData = {
+  name: "",
+  number: "",
+  flat: "",
+  area: "",
+  landmark: "",
+  pincode: "",
+  city: "",
+  state: "",
+};
+
 function Shipment() {
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  function handleChange(e) {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    // console.log(e.target);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  console.log(formData);
   return (
     <div className={styles.shipment_container}>
       <div></div>
       <div className={styles.form_container}>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div>
             <label>Full Name</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Mobile Number</label>
-            <input type="text" />
+            <input
+              type="number"
+              name="number"
+              value={formData.number}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Flat, House no., Building, Company, Apartment</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="flat"
+              value={formData.flat}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Area, Street, Sector, Village</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="area"
+              value={formData.area}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Landmark</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="landmark"
+              value={formData.landmark}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Pincode</label>
-            <input type="number" />
+            <input
+              type="number"
+              name="pincode"
+              value={formData.pincode}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>Town/City</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={(e) => handleChange(e)}
+            />
           </div>
           <div>
             <label>State</label>
-            <select>
-              <option>Choose a state</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
+            <select
+              name="state"
+              value={formData.state}
+              onChange={(e) => handleChange(e)}
+            >
+              <option value="Andhra Pradesh">Andhra Pradesh</option>
+              <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+              <option value="Assam">Assam</option>
+              <option value="Bihar">Bihar</option>
+              <option value="Chhattisgarh">Chhattisgarh</option>
+              <option value="Goa">Goa</option>
+              <option value="Gujarat">Gujarat</option>
+              <option value="Haryana">Haryana</option>
+              <option value="Himachal Pradesh">Himachal Pradesh</option>
+              <option value="Jharkhand">Jharkhand</option>
+              <option value="Karnataka">Karnataka</option>
+              <option value="Kerala">Kerala</option>
+              <option value="Madhya Pradesh">Madhya Pradesh</option>
+              <option value="Maharashtra">Maharashtra</option>
+              <option value="Manipur">Manipur</option>
+              <option value="Meghalaya">Meghalaya</option>
+              <option value="Mizoram">Mizoram</option>
+              <option value="Nagaland">Nagaland</option>
+              <option value="Odisha">Odisha</option>
+              <option value="Punjab">Punjab</option>
+              <option value="Rajasthan">Rajasthan</option>
+              <option value="Sikkim">Sikkim</option>
+              <option value="Tamil Nadu">Tamil Nadu</option>
+              <option value="Telangana">Telangana</option>
+              <option value="Tripura">Tripura</option>
+              <option value="Uttar Pradesh">Uttar Pradesh</option>
+              <option value="Uttarakhand">Uttarakhand</option>
+              <option value="West Bengal">West Bengal</option>
+              <option value="Andaman and Nicobar Islands">
+                Andaman and Nicobar Islands
+              </option>
+              <option value="Chandigarh">Chandigarh</option>
+              <option value="Dadra and Nagar Haveli and Daman and Diu">
+                Dadra and Nagar Haveli and Daman and Diu
+              </option>
+              <option value="Lakshadweep">Lakshadweep</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Puducherry">Puducherry</option>
+              <option value="Ladakh">Ladakh</option>
+              <option value="Jammu and Kashmir">Jammu and Kashmir</option>
             </select>
           </div>
           <button>Add address</button>
