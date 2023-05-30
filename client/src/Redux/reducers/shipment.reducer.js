d01a73941a5d48a4c1f6056082dabe40cd5481ee
shubham kumar <kumarshubham417@gmail.com>
2023-05-30 18:48:04 +0530

    shipment reducer created

---
+++ client/src/Redux/reducers/shipment.reducer.js
@@ -0,0 +1,70 @@
+import * as actionTypes from "../actionTypes/shipment.actionTypes";
+
+const initialState = {
+  addresses: [],
+  loading: false,
+  error: null,
+};
+
+const shipmentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_ADDRESS_REQUEST:
+    case actionTypes.GET_ADDRESS_REQUEST:
+    case actionTypes.UPDATE_ADDRESS_REQUEST:
+    case actionTypes.REMOVE_ADDRESS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case actionTypes.ADD_ADDRESS_SUCCESS:
+      return {
+        ...state,
+        addresses: [...state.addresses, action.payload],
+        loading: false,
+        error: null,
+      };
+    case actionTypes.GET_ADDRESS_SUCCESS:
+      return {
+        ...state,
+        addresses: action.payload,
+        loading: false,
+        error: null,
+      };
+    case actionTypes.UPDATE_ADDRESS_SUCCESS:
+      const updatedAddresses = state.addresses.map((address) =>
+        address._id === action.payload._id
+          ? { ...address, ...action.payload.newAddress }
+          : address
+      );
+      return {
+        ...state,
+        addresses: updatedAddresses,
+        loading: false,
+        error: null,
+      };
+    case actionTypes.REMOVE_ADDRESS_SUCCESS:
+      const filteredAddresses = state.addresses.filter(
+        (address) => address._id !== action.payload
+      );
+      return {
+        ...state,
+        addresses: filteredAddresses,
+        loading: false,
+        error: null,
+      };
+    case actionTypes.ADD_ADDRESS_FAILURE:
+    case actionTypes.GET_ADDRESS_FAILURE:
+    case actionTypes.UPDATE_ADDRESS_FAILURE:
+    case actionTypes.REMOVE_ADDRESS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export { shipmentReducer };
