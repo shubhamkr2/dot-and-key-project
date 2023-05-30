6a04f445e01fb3a906b50873f3ffd46f59c26358
shubham kumar <kumarshubham417@gmail.com>
2023-05-30 16:55:18 +0530

    shipment form created

--- client/src/Pages/Shipment.jsx
+++ client/src/Pages/Shipment.jsx
@@ -5,29 +5,45 @@ function Shipment() {
   return (
     <div className={styles.shipment_container}>
       <div></div>
-      <div>
+      <div className={styles.form_container}>
         <form>
-          <label>Full Name</label>
-          <input type="text" />
-          <label>Mobile Number</label>
-          <input type="text" />
-          <label>Flat, House no., Building, Company, Apartment</label>
-          <input type="text" />
-          <label>Area, Street, Sector, Village</label>
-          <input type="text" />
-          <label>Landmark</label>
-          <input type="text" />
-          <label>Pincode</label>
-          <input type="number" />
-          <label>Town/City</label>
-          <input type="text" />
-          <label>State</label>
-          <select>
-            <option>Choose a state</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </select>
+          <div>
+            <label>Full Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Mobile Number</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Flat, House no., Building, Company, Apartment</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Area, Street, Sector, Village</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Landmark</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Pincode</label>
+            <input type="number" />
+          </div>
+          <div>
+            <label>Town/City</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>State</label>
+            <select>
+              <option>Choose a state</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+          </div>
           <button>Add address</button>
         </form>
       </div>
