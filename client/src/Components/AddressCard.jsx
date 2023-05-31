235a610cbb2177a8d1da697c162dab64330b9cb8
shubham kumar <kumarshubham417@gmail.com>
2023-05-31 14:36:18 +0530

    AddressCard updated

--- client/src/Components/AddressCard.jsx
+++ client/src/Components/AddressCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import styles from "../Styles/Addreess.module.css";
+import styles from "../Styles/AddreessCard.module.css";
 function AddressCard({ address }) {
   const { area, city, flat, landmark, name, number, pincode, state } = address;
   return (
     <div className={styles.address_card}>
       <h2>Name: {name}</h2>
-      <h3>Area: {area}</h3>
-      <h3>Pincode- {pincode}</h3>
-      <h3>Mob. No.- {number}</h3>
-      <h3>City: {city}</h3>
-      <h3>State: {state}</h3>
+      <p>Area: {area}</p>
+      <div>
+        <span>City: {city}</span>
+        <span>Pincode: {pincode}</span>
+      </div>
+      <div>
+        <span>Mobile: {number}</span>
+        <state>State: {state}</state>
+      </div>
     </div>
   );
 }
