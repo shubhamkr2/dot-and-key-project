25bfa98c27f28ae423280efb06b9722ac372ce3f
shubham kumar <kumarshubham417@gmail.com>
2023-06-01 18:43:45 +0530

    address card  updated

--- client/src/Components/AddressCard.jsx
+++ client/src/Components/AddressCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "../Styles/AddressCard.module.css";
+
 function AddressCard({ Address }) {
   const { area, city, flat, landmark, name, number, pincode, state } = Address;
   return (
     <div className={styles.address_card}>
       <input type="radio" />
-      <span>{name}</span>
-      <span>{flat}, {area}, {landmark}, {pincode}, {city}, {state} </span>
+      <span>
+        <strong>{name}</strong> {flat}, {area}, {landmark}, {pincode}, {city}, {state}, Phone number: {number}
+      </span>
     </div>
   );
 }
