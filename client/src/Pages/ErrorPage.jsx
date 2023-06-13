3e18381ea80335c243077ef0a802f52e8068925e
shubham kumar <kumarshubham417@gmail.com>
2023-06-13 13:49:52 +0530

    error page width fixed

--- client/src/Pages/ErrorPage.jsx
+++ client/src/Pages/ErrorPage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { NavigationBar } from "../Components/NavigationBar";
+import { Footer } from "../Components/Footer";
 
 function ErrorPage() {
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div style={{maxWidth: "1024px",margin:"auto", backgroundColor:"white"}}>
+    <NavigationBar />
+    <div style={{  display: "flex", justifyContent: "center", alignItems: "center" }}>
       <img
-        style={{ width: "80dvw", margin: "auto" }}
+        style={{ width: "100%", margin: "auto" }}
         src="https://blog.thomasnet.com/hubfs/shutterstock_774749455.jpg"
         alt="error"
       />
+    </div>
+    <Footer />
     </div>
   );
 }
