9a99692a2ac6eff9dec36f874c45b051fcb38c5e
shubham kumar <kumarshubham417@gmail.com>
2023-06-15 23:49:33 +0530

    account.jsx updated

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 import styles from "../Styles/Account.module.css";
+
 function Account() {
+  const name = localStorage.getItem("user_name") || "";
+
   return (
     <div className={styles.container}>
       <h1 className={styles.category}>My account</h1>
+      <div>
+        <h2>Name: {name}</h2>
+      </div>
     </div>
-  )
+  );
 }
 
 export { Account };
