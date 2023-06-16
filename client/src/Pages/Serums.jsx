f12076a7a615edc642bb4a09a06dc01ff038734f
shubham kumar <kumarshubham417@gmail.com>
2023-06-16 20:20:41 +0530

    Serums page updated with comment

--- client/src/Pages/Serums.jsx
+++ client/src/Pages/Serums.jsx
@@ -3,7 +3,6 @@ import styles from "../Styles/Serums.module.css";
 import { NavigationBar } from "../Components/NavigationBar";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProduct } from "../Redux/actions/product.action";
-import { useParams } from "react-router-dom";
 import ProductCard from "../Components/ProductCard";
 import { Footer } from "../Components/Footer";
 import Pagination from "../Components/Pagination";
@@ -17,12 +16,10 @@ function Serums() {
   const [sortAs, setSortAs] = useState("");
   const [filterAsRating, setFilterAsRating] = useState("");
   const dispatch = useDispatch();
-  const url = useParams();
 
   useEffect(() => {
-    dispatch(getAllProduct("serums", page, sortAs, filterAsRating)).then(
-      () => {}
-    );
+    // Fetch serum products based on page, sortAs, and filterAsRating values
+    dispatch(getAllProduct("serums", page, sortAs, filterAsRating)).then(() => {});
   }, [page, sortAs, filterAsRating]);
 
   console.log(data);
@@ -40,21 +37,22 @@ function Serums() {
       <h1 className={styles.category}>Serums</h1>
       <div className={styles.sidebar_and_product}>
         <div className={styles.Sidebar}>
-          <Sidebar
-            setSortAs={setSortAs}
-            setFilterAsRating={setFilterAsRating}
-          />
+          {/* Sidebar component for sorting and filtering options */}
+          <Sidebar setSortAs={setSortAs} setFilterAsRating={setFilterAsRating} />
         </div>
         {loading ? (
+          // Skeleton component for showing loading state
           <Skeleton />
         ) : (
           <div className={styles.product_list}>
+            {/* Render product cards for each serum product */}
             {data?.data?.map((product) => (
               <ProductCard product={product} key={product._id} toast={toast} />
             ))}
           </div>
         )}
       </div>
+      {/* Pagination component for navigating through product pages */}
       <Pagination page={page} setPage={setPage} data={data} />
       <Footer />
     </div>
