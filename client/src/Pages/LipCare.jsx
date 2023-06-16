cdc749ae2ae04260862d46ca98eb2125fba1b92d
shubham kumar <kumarshubham417@gmail.com>
2023-06-16 20:07:33 +0530

    lipcare page updated with comment

--- client/src/Pages/LipCare.jsx
+++ client/src/Pages/LipCare.jsx
@@ -3,7 +3,6 @@ import styles from "../Styles/LipCare.module.css";
 import { NavigationBar } from "../Components/NavigationBar";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProduct } from "../Redux/actions/product.action";
-import { useParams } from "react-router-dom";
 import ProductCard from "../Components/ProductCard";
 import { Footer } from "../Components/Footer";
 import Pagination from "../Components/Pagination";
@@ -12,20 +11,21 @@ import { Skeleton } from "../Components/Skeleton";
 import toast, { Toaster } from "react-hot-toast";
 
 function LipCare() {
+  // Retrieve 'loading' and 'data' from the product state in Redux store
   const { loading, data } = useSelector((state) => state.product);
-  const [page, setPage] = useState(1);
-  const [sortAs, setSortAs] = useState("");
-  const [filterAsRating, setFilterAsRating] = useState("");
+
+  // Define state variables
+  const [page, setPage] = useState(1); // Current page number
+  const [sortAs, setSortAs] = useState(""); // Sort order
+  const [filterAsRating, setFilterAsRating] = useState(""); // Rating filter
+
   const dispatch = useDispatch();
-  const url = useParams();
 
   useEffect(() => {
-    dispatch(getAllProduct("lipcare", page, sortAs, filterAsRating)).then(
-      () => {}
-    );
+    // Dispatch action to fetch products with the given parameters
+    dispatch(getAllProduct("lipcare", page, sortAs, filterAsRating));
   }, [page, sortAs, filterAsRating]);
 
-  console.log(data);
   return (
     <div className={styles.lipcare_container}>
       <NavigationBar />
@@ -40,21 +40,21 @@ function LipCare() {
       <h1 className={styles.category}>LipCare</h1>
       <div className={styles.sidebar_and_product}>
         <div className={styles.Sidebar}>
-          <Sidebar
-            setSortAs={setSortAs}
-            setFilterAsRating={setFilterAsRating}
-          />
+          {/* Render the Sidebar component and pass the setter functions for sortAs and filterAsRating */}
+          <Sidebar setSortAs={setSortAs} setFilterAsRating={setFilterAsRating} />
         </div>
         {loading ? (
-          <Skeleton />
+          <Skeleton /> // Render skeleton component while loading
         ) : (
           <div className={styles.product_list}>
+            {/* Iterate over the product data and render ProductCard component for each product */}
             {data?.data?.map((product) => (
-              <ProductCard product={product} key={product._id} toast={toast}/>
+              <ProductCard product={product} key={product._id} toast={toast} />
             ))}
           </div>
         )}
       </div>
+      {/* Render Pagination component and pass page, setPage, and data as props */}
       <Pagination page={page} setPage={setPage} data={data} />
       <Footer />
     </div>
