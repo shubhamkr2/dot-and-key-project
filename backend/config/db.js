af69672d524e641d6eec2f16fe19ea4d224fa6cf
shubham kumar <kumarshubham417@gmail.com>
2023-06-24 13:40:57 +0530

    config file updated with async

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.MONGODB_URL);
+const connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to DB");
+  } catch (err) {
+    console.error("Connection to DB failed:", err);
+  }
+};
 
 module.exports = { connection };
