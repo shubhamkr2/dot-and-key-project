e6c443e0b9864fe7d1331483ce2edd3859e3b15c
shubham kumar <kumarshubham417@gmail.com>
2023-06-25 22:08:39 +0530

    middleware updated

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,14 +3,30 @@ require("dotenv").config();
 
 const authenticate = (req, res, next) => {
   const token = req.headers.authorization;
-  //   console.log(token)
-  if (token) {
-    let temp = token.trim().split(" ");
-    const decoded = jwt.verify(temp[1], process.env.JWT_SECRET);
+
+  // Check if token is provided
+  if (!token) {
+    return res.status(401).json({ message: "Token not provided" });
+  }
+
+  // Split the token string into bearer prefix and token value
+  const [bearerPrefix, tokenValue] = token.trim().split(" ");
+
+  // Check if token value and bearer prefix are valid
+  if (!tokenValue || bearerPrefix.toLowerCase() !== "bearer") {
+    return res.status(401).json({ message: "Invalid token format" });
+  }
+
+  try {
+    // Verify and decode the token
+    const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET);
+
+    // Set the decoded user ID in the request body
     req.body.userId = decoded.id;
     next();
-  } else {
-    res.status(401).json({ message: "token not found" });
+  } catch (error) {
+    // Handle invalid token
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
