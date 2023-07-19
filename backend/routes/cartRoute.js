961f336aab3433b1e0c22b80ca9cad815aa33911
shubham kumar <kumarshubham417@gmail.com>
2023-07-19 18:52:14 +0530

    cartRoute updated

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -5,6 +5,7 @@ const {
   addItem,
   updateItem,
   deleteItem,
+  deleteAllItems,
 } = require("../controlers/cartController");
 const { authenticate } = require("../middlewares/authMiddleware");
 
@@ -15,5 +16,6 @@ cartRoute.get("/:id", authenticate, getItemByID);
 cartRoute.post("/", authenticate, addItem);
 cartRoute.put("/:id", authenticate, updateItem);
 cartRoute.delete("/:id", authenticate, deleteItem);
+cartRoute.delete("/", authenticate, deleteAllItems);
 
 module.exports = { cartRoute };
